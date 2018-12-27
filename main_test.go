6695ef6a6afa4832cdb030480f4e0ccda84cff12
mr-karan <karansharma1295@gmail.com>
2018-12-27 10:49:57 +0530

    Add tests file

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+)
+
+// TestHealthCheckHandler checks if /ping route returns 200 OK
+func TestHealthCheckHandler(t *testing.T) {
+
+}
+
+// TestIndexHandler checks if / (index) route returns 200 OK
+func TestIndexHandler(t *testing.T) {
+
+}
