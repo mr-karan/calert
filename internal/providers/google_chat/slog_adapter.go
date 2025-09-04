f779e65d9c891d4ee038ec14fb88ebf93cf28914
Ryu Sato <ryu@weseek.co.jp>
2025-09-04 23:08:49 +0900

    docs: add comments for slogAdapter implementation details

--- internal/providers/google_chat/slog_adapter.go
+++ internal/providers/google_chat/slog_adapter.go
@@ -5,10 +5,13 @@ import (
 	"log/slog"
 )
 
+// slogAdapter implements the retryablehttp.LeveledLogger interface using slog.
 type slogAdapter struct {
 	logger *slog.Logger
 }
 
+// Implements for the retryablehttp.LeveledLogger
+// ref. https://pkg.go.dev/github.com/hashicorp/go-retryablehttp#LeveledLogger
 func (adpt *slogAdapter) Error(msg string, keysAndValues ...interface{}) {
 	adpt.logger.Error(msg, keysAndValues...)
 }
@@ -25,6 +28,8 @@ func (adpt *slogAdapter) Warn(msg string, keysAndValues ...interface{}) {
 	adpt.logger.Warn(msg, keysAndValues...)
 }
 
+// Implements for the retryablehttp.Logger
+// ref. https://pkg.go.dev/github.com/hashicorp/go-retryablehttp#Logger
 func (adpt *slogAdapter) Printf(format string, args ...interface{}) {
 	adpt.logger.Info(fmt.Sprintf(format, args...))
 }
